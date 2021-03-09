b54a6072c525d9d46906ee60f1836a1a9fdd1f69
Luis <luis.salazar@anses.fr>
2021-03-09 11:45:01 +0100

    Update List includes now both: All iterations and quantile-median graphics

--- Outputs/AnalyseParallel.R
+++ Outputs/AnalyseParallel.R
@@ -9,21 +9,24 @@ library(survival)
 
 # List of Output Graphs ---------------------------------------------------
 
-Scenario = 1:4
+Scenario = 1:8
 #par(mfrow = c(2, 1))
 ScenarioTable <- NULL
 OutPutGraph   <- NULL
 
 for (Scen in Scenario) {
-  if(Scen %in% c(1:2)) load("X:/Luis/Model/GIT/WildBoar_Model/Inputs/habitats_Dep64.RData") else
+  if(Scen %in% c(1:2, 5:6)) load("X:/Luis/Model/GIT/WildBoar_Model/Inputs/habitats_Dep64.RData") else
   load("X:/Luis/Model/GIT/WildBoar_Model/Inputs/habitats_EA9.RData")
   
   #Study Area
-  if(Scen %in% c(1:2)) Study_Area = "Department_64" else 
+  if(Scen %in% c(1:2, 5:6)) Study_Area = "Department_64" else 
                            Study_Area = "France-Belgium_Border"
   
   #Details
-  if(Scen %in% c(1, 3)) Details = "Without Hunting" else Details = "Hunting Season"
+  if(Scen %in% c(1, 3, 5, 7)) Details = "Without Hunting" else Details = "Hunting Season"
+  
+  #Season Introduction
+  if(Scen %in% c(1:4)) Season = "Winter" else Season = "Summer"
 
   #Surface in km2
   gArea(habitats)/1e6 -> surface
@@ -73,52 +76,69 @@ for (Scen in Scenario) {
     c(unique(x$Iter), max(x$gTime) - 750)
     }) %>% do.call(rbind,.)
  
- # All Iterations Graphic 
- # PPopulation <- ggplot(Population, aes(x = Time, y = value, color = variable)) +
- #                        ggtitle(label = Study_Area, subtitle = paste("Population", Details)) + 
- #                        geom_line() + theme(legend.position = "none") 
-            
- PPopulation <- ggplot(Population, aes(x = Time, y = value)) +
-                ggtitle(label = Study_Area, subtitle = paste("Population", Details)) + 
-                geom_line(colour = "cornflowerblue", alpha = 0.3) + theme(legend.position = "none") + 
+ ### All Iterations Graphic 
+ PPopulationA <- ggplot(Population, aes(x = Time, y = value, color = variable)) +
+                ggtitle(label = Study_Area, subtitle = paste("Population", "/", Details, "/", Season)) +
+                geom_line() + theme(legend.position = "none")
+    
+ ### Median and quantile plots        
+ PPopulationQ <- ggplot(Population, aes(x = Time, y = value)) +
+                ggtitle(label = Study_Area, subtitle = paste("Population", "/", Details, "/", Season)) +
+                geom_line(colour = "cornflowerblue", alpha = 0.3) + theme(legend.position = "none") +
                 stat_summary(fun.min = function(z) { quantile(z,0.25) },
                              fun.max = function(z) { quantile(z,0.75) },
                              fun = median, geom = "smooth", colour = "darksalmon", size = 1)
   
-     PDensity <- ggplot(Density, aes(x = Time, y = density)) +
-                 ggtitle(label = Study_Area, subtitle = paste("WB Density", Details)) + 
-                 geom_line(colour = "cornflowerblue", alpha = 0.3) + theme(legend.position = "none") + 
+  PDensityA <- ggplot(Density, aes(x = Time, y = density, color = variable)) +
+              ggtitle(label = Study_Area, subtitle = paste("WB Density", "/", Details, "/", Season)) +
+              geom_line() + theme(legend.position = "none")
+
+  PDensityQ <- ggplot(Density, aes(x = Time, y = density)) +
+                 ggtitle(label = Study_Area, subtitle = paste("WB Density", "/", Details, "/", Season)) +
+                 geom_line(colour = "cornflowerblue", alpha = 0.3) + theme(legend.position = "none") +
                  stat_summary(fun.min = function(z) { quantile(z,0.25) },
                               fun.max = function(z) { quantile(z,0.75) },
                               fun = median, geom = "smooth", colour = "coral", size = 1)
-   
- 
-   PInfected <- ggplot(Infected, aes(x = Time, y = value)) +
-                ggtitle(label = Study_Area, subtitle = paste("Infected WB", Details)) + 
-                geom_line(colour = "cornflowerblue", alpha = 0.3) + theme(legend.position = "none") + 
+
+  PInfectedA <- ggplot(Infected, aes(x = Time, y = value, color = variable)) +
+                ggtitle(label = Study_Area, subtitle = paste("Infected WB", "/", Details, "/", Season)) +
+                geom_line() + theme(legend.position = "none")
+  
+  PInfectedQ <- ggplot(Infected, aes(x = Time, y = value)) +
+                ggtitle(label = Study_Area, subtitle = paste("Infected WB", "/", Details, "/", Season)) +
+                geom_line(colour = "cornflowerblue", alpha = 0.3) + theme(legend.position = "none") +
                 stat_summary(fun.min = function(z) { quantile(z,0.25) },
                   fun.max = function(z) { quantile(z,0.75) },
                   fun = median, geom = "smooth", colour = "brown2", size = 1)
  
-   PCarcass <- ggplot(Carcasses, aes(x = Time, y = value)) +
-               ggtitle(label = Study_Area, subtitle = paste("Infected Carcasses", Details)) + 
-               geom_line(colour = "cornflowerblue", alpha = 0.3) + theme(legend.position = "none") + 
+  PCarcassA <- ggplot(Carcasses, aes(x = Time, y = value, color = variable)) +
+               ggtitle(label = Study_Area, subtitle = paste("Infected Carcasses", "/", Details, "/", Season)) +
+               geom_line() + theme(legend.position = "none")
+  
+  PCarcassQ <- ggplot(Carcasses, aes(x = Time, y = value)) +
+               ggtitle(label = Study_Area, subtitle = paste("Infected Carcasses", "/", Details, "/", Season)) +
+               geom_line(colour = "cornflowerblue", alpha = 0.3) + theme(legend.position = "none") +
                stat_summary(fun.min = function(z) { quantile(z,0.25) },
                             fun.max = function(z) { quantile(z,0.75) },
                             fun = median, geom = "smooth", colour = "darkgoldenrod2", size = 1)
  
  ScenarioTable <- rbind(ScenarioTable, c(Scenario   = Scen,
                                          Study_Area = Study_Area,
-                                         Details    = Details))
+                                         Details    = Details,
+                                         Season     = Season))
  
- OutPutGraph[[Scen]] <- list(PPopulation, PDensity, PInfected, PCarcass, EpiDuration)
+ OutPutGraph[[Scen]] <- list(PPopulationA, PPopulation,
+                             PDensityA, PDensityQ,
+                             PInfectedA, PInfectedQ,
+                             PCarcassA, PCarcassQ,
+                             EpiDuration)
    
 }
 
-# OutPutGraph[[Scenario]][Graph]
+# OutPutGraph[[Scenario]][Graph/Table]
 # Plot all Graphs per Scenario
 
-OutPutGraph[[1]][[2]]
+OutPutGraph[[5]][[10]]
 
 
 
@@ -127,14 +147,19 @@ OutPutGraph[[1]][[2]]
 # S<-Surv(EpiDuration[ ,2],event = rep(1, 100))
 # plot(survfit(S ~ 1))
 
-ED1 <- as.data.frame(OutPutGraph[[1]][[5]]) %>% mutate(Scenario = 1) %>% rename(Iter = V1, EpDuration = V2)
-ED2 <- as.data.frame(OutPutGraph[[2]][[5]]) %>% mutate(Scenario = 2) %>% rename(Iter = V1, EpDuration = V2)
-ED3 <- as.data.frame(OutPutGraph[[3]][[5]]) %>% mutate(Scenario = 3) %>% rename(Iter = V1, EpDuration = V2)
-ED4 <- as.data.frame(OutPutGraph[[4]][[5]]) %>% mutate(Scenario = 4) %>% rename(Iter = V1, EpDuration = V2)
+ED1 <- as.data.frame(OutPutGraph[[1]][[9]]) %>% mutate(Scenario = 1) %>% rename(Iter = V1, EpDuration = V2)
+ED2 <- as.data.frame(OutPutGraph[[2]][[9]]) %>% mutate(Scenario = 2) %>% rename(Iter = V1, EpDuration = V2)
+ED3 <- as.data.frame(OutPutGraph[[3]][[9]]) %>% mutate(Scenario = 3) %>% rename(Iter = V1, EpDuration = V2)
+ED4 <- as.data.frame(OutPutGraph[[4]][[9]]) %>% mutate(Scenario = 4) %>% rename(Iter = V1, EpDuration = V2)
+ED5 <- as.data.frame(OutPutGraph[[5]][[9]]) %>% mutate(Scenario = 5) %>% rename(Iter = V1, EpDuration = V2)
+ED6 <- as.data.frame(OutPutGraph[[6]][[9]]) %>% mutate(Scenario = 6) %>% rename(Iter = V1, EpDuration = V2)
+ED7 <- as.data.frame(OutPutGraph[[7]][[9]]) %>% mutate(Scenario = 7) %>% rename(Iter = V1, EpDuration = V2)
+ED8 <- as.data.frame(OutPutGraph[[8]][[9]]) %>% mutate(Scenario = 8) %>% rename(Iter = V1, EpDuration = V2)
 
-EDT <- bind_rows(ED1, ED2, ED3, ED4)
+EDT <- bind_rows(ED1, ED2, ED3, ED4, ED5, ED6, ED7, ED8)
+ED <- bind_rows(ED5, ED6, ED7, ED8)
 
-survfit(Surv(EDT[ ,"EpDuration"], event = rep(1, 400)) ~Scenario, data = EDT) -> a
-plot(a, col = 1:4, main = "Scenarios Comparison", xlab = "Time", ylab = "Probability of infection")
-legend('topright', legend = levels(factor(EDT$Scenario)), lty = 1, col = 1:4)
+survfit(Surv(EDT[ ,"EpDuration"], event = rep(1, 800)) ~Scenario, data = EDT) -> a
+plot(a, col = 1:8, main = "Scenarios Comparison Summer vs Winter Intro", xlab = "Time", ylab = "Probability of infection")
+legend('topright', legend = levels(factor(EDT$Scenario)), lty = 1, col = 1:8)
 
